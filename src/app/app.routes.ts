216ca05cdbe991fb9b940c2d62936a2c4b0cd62b
TuongVan27 <tvaan278@gmail.com>
2025-06-09 16:22:33 +0700

    Giao diện trang log-in + chỉnh sửa sign-up - tuongvan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { SignUpComponent } from './client/pages/sign-up/sign-up.component';
+import { LogInComponent } from './client/pages/log-in/log-in.component';
 
 export const routes: Routes = [
-    {path:'sign-up', component:SignUpComponent, title:'Đăng nhập'}
+    {path:'sign-up', component:SignUpComponent, title:'Đăng ký'},
+    {path:'log-in', component:LogInComponent, title:'Đăng nhập'},
 ];
