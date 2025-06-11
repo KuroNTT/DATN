430b1f2edb619315c9d9914cdb60891ff1f05d3a
TuongVan27 <tvaan278@gmail.com>
2025-06-11 22:43:45 +0700

    Giao diện trang liên hệ (contact) - tuongvan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignUpComponent } from './client/pages/sign-up/sign-up.component';
 import { LogInComponent } from './client/pages/log-in/log-in.component';
+import { ContactComponent } from './client/pages/contact/contact.component';
 
 export const routes: Routes = [
     {path:'sign-up', component:SignUpComponent, title:'Đăng ký'},
     {path:'log-in', component:LogInComponent, title:'Đăng nhập'},
+    {path:'contact', component:ContactComponent, title:'Liên hệ'},
 ];
