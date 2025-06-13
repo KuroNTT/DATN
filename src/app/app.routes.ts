1dfaff6116d843d1bfe9171195b9315ac7de4af5
TuongVan27 <tvaan278@gmail.com>
2025-06-13 20:58:06 +0700

    Trang đang nhập đăng ký responsive - tuongvan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,6 @@ import { ContactComponent } from './client/pages/contact/contact.component';
 
 export const routes: Routes = [
     {path:'sign-up', component:SignUpComponent, title:'Đăng ký'},
-    {path:'log-in', component:LogInComponent, title:'Đăng nhập'},
+    {path:'sign-in', component:LogInComponent, title:'Đăng nhập'},
     {path:'contact', component:ContactComponent, title:'Liên hệ'},
 ];
