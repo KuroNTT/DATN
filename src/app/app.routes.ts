5ea86c55454c62963dbae5783fef26806ba775a4
levuminh08062000 <113541947+levuminh08062000@users.noreply.github.com>
2025-06-14 19:48:57 +0700

    VMinh - Push trang  product-detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,21 @@
-import { Routes } from '@angular/router';
-
+import { Routes } from "@angular/router";
+import { ProductDetailComponent } from "./client/pages/product-detail/product-detail.component";
 export const routes: Routes = [
-    {path: '', loadChildren: ()=>import('./client/client.module').then(m=>m.ClientModule)},
-    {path: 'admin', loadChildren: ()=>import('./admin/admin.module').then(m=>m.AdminModule)},
-    {path: '**', redirectTo:''}
+  {
+    path: "",
+    loadChildren: () =>
+      import("./client/client.module").then((m) => m.ClientModule),
+  },
+  {
+    path: "product-detail",
+    component: ProductDetailComponent,
+    title: "Chi tiết sản phẩm",
+  },
+  {
+    path: "admin",
+    loadChildren: () =>
+      import("./admin/admin.module").then((m) => m.AdminModule),
+  },
+
+  { path: "**", redirectTo: "" },
 ];
--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,14 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { ClientRoutingModule } from './client-routing.module';
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ProductDetailComponent } from "./pages/product-detail/product-detail.component";
+import { ClientRoutingModule } from "./client-routing.module";
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    ClientRoutingModule
-  ]
+  imports: [CommonModule, ClientRoutingModule, ProductDetailComponent],
 })
-export class ClientModule { }
+export class ClientModule {}
