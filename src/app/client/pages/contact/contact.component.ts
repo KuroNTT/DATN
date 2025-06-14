7bf2f12ad94a203dff08e37882ab6c7178f85d5d
ChiToan <167092655+ChiToan2412@users.noreply.github.com>
2025-06-14 23:20:06 +0700

    chitoan: giao diện trang liên hệ

--- src/app/client/pages/contact/contact.component.ts
+++ src/app/client/pages/contact/contact.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
+const useMaterial = [
+  MatInputModule,
+  MatFormFieldModule,
+  FormsModule
+];
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [...useMaterial],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
