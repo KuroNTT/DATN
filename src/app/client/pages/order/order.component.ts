75fcc3bf8b7ccb5d02b79d33cef0e0c728e97d01
ChiToan <167092655+ChiToan2412@users.noreply.github.com>
2025-06-15 19:18:55 +0700

    chitoan: giao dien trang order

--- src/app/client/pages/order/order.component.ts
+++ src/app/client/pages/order/order.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
+const matAngular = [
+  MatButtonModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule
+];
 @Component({
   selector: 'app-order',
-  imports: [],
+  imports: [...matAngular],
   templateUrl: './order.component.html',
   styleUrl: './order.component.css'
 })
