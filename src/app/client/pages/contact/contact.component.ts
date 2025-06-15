9cf4dbc0e84ef36390b1905be22fb343f4443398
ChiToan <167092655+ChiToan2412@users.noreply.github.com>
2025-06-15 18:22:38 +0700

    chitoan: update

--- src/app/client/pages/contact/contact.component.ts
+++ src/app/client/pages/contact/contact.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
 
-const useMaterial = [
-  MatInputModule,
-  MatFormFieldModule,
-  FormsModule
-];
 @Component({
   selector: 'app-contact',
-  imports: [...useMaterial],
+  imports: [],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
