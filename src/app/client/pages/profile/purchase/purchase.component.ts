b4944d6560fde0fea4167129cb34584dc102c181
TuongVan27 <tvaan278@gmail.com>
2025-06-28 19:41:17 +0700

    Chỉnh sửa rss cho trang pf/purchase - tuongvan

--- src/app/client/pages/profile/purchase/purchase.component.ts
+++ src/app/client/pages/profile/purchase/purchase.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-purchase',
   imports: [],
-  templateUrl: './purchase.component.html'
+  templateUrl: './purchase.component.html',
+  styleUrl: './purchase.component.css',
+
 })
 export class PurchaseComponent {
 
